c0c8a2fd6687edcd8fdb0a888c8cbec1b4ab9ef6
Kulik Vyacheslav <kulik.vyacheslav94@gmail.com>
2021-03-12 09:55:31 +0600

    Adding organizationPublicId to header + copy button

--- src/components/grid/Header/index.tsx
+++ src/components/grid/Header/index.tsx
@@ -1,5 +1,5 @@
 import { CustomImg } from 'components/common/imageComponents/CustomImg';
-import { headerLogoHeight } from 'components/grid/Header/constants';
+import { headerCopyButtonDiameter, headerLogoHeight } from 'components/grid/Header/constants';
 import { StyledHeader, StyledSpan1, StyledSpan2, StyledSpan3 } from 'components/grid/Header/style';
 import { Column, Row } from 'components/grid/wrappers/FlexWrapper';
 import { siteName } from 'constants/global';
@@ -9,6 +9,9 @@ import { useStore } from 'effector-react';
 import React from 'react';
 import { themeStores } from 'stores/theme';
 import { userEvents, userStores } from 'stores/user';
+import copyButtonIcon from 'assets/img/copy_button_icon.svg';
+import { ImgButton } from 'components/common/buttons/ImgButton';
+import { MarginWrapper } from '../wrappers/MarginWrapper';
 
 export const Header = () => {
     const { user } = useStore(userStores.user);
@@ -16,6 +19,9 @@ export const Header = () => {
 
     const username = user && user?.username;
     // const imageUrl = user && user?.profile?.imageUrl;
+
+    //MOCK
+    const organizationPublicId = '123123sdasd123';
 
     const onClick = () => userEvents.logout();
 
@@ -26,8 +32,21 @@ export const Header = () => {
                 <StyledSpan1 color={white}>{siteName}</StyledSpan1>
             </Row>
             <Row alignCenter marginBottom="0">
-                <Column marginRight={padding}>
-                    <StyledSpan2 color={white}>{username ? username : noContentMessage}</StyledSpan2>
+                <Column alignCenter marginRight={padding}>
+                    <Row>
+                        <StyledSpan2 color={white}>
+                            {username ? `${username} (${organizationPublicId})` : noContentMessage}
+                        </StyledSpan2>
+                        <MarginWrapper marginBottom="8px" marginLeft="10px">
+                            <ImgButton
+                                backgroundColor="#272727"
+                                height={headerCopyButtonDiameter}
+                                width={headerCopyButtonDiameter}
+                            >
+                                <CustomImg height="13px" src={copyButtonIcon} width="13px" />
+                            </ImgButton>
+                        </MarginWrapper>
+                    </Row>
                     <StyledSpan3 color={white} onClick={onClick}>
                         Logout
                     </StyledSpan3>
