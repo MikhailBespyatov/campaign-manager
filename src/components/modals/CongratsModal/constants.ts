c1852716a998d1bbcbe6eaf6c93ecc175297e04f
Вячеслав <vaceslav@MacBook-Pro-Vaceslav.local>
2021-06-08 12:10:02 +0600

    Changing async modal height

--- src/components/modals/CongratsModal/constants.ts
+++ src/components/modals/CongratsModal/constants.ts
@@ -2,7 +2,7 @@ export const wrapperBackground = 'rgba(0, 0, 0, 0.612407)';
 export const wrapperVerticalPadding = '90px';
 export const wrapperHorizontalPadding = '63px';
 
-export const modalWidth = '420px'; /* '1050px'; */
+export const modalWidth = '650px'; /* '1050px'; */
 export const modalHeight = '440px'; /* '370px'; */
 export const modalBoxShadow = '0px 2px 8px rgba(0, 0, 0, 0.0778245);';
 
