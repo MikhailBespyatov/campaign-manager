97b79cdd48fd92ca3b6a751127ac43e8524c099c
Татьяна Лепестова <tatiana.lepestova@gmail.com>
2021-06-10 03:46:28 +0000

    (feat) YW-154: Updated design of WalletModal

--- src/components/common/features/CopyableField/index.tsx
+++ src/components/common/features/CopyableField/index.tsx
@@ -1,13 +1,13 @@
-import { BackgroundColor, Color } from 'types';
-import { blue, blue4, blue5 } from 'constants/styles';
-import { CopyableFieldSpan, CopyableFieldWrapper } from './styles';
-import { MarginWrapper } from 'components/grid/wrappers/MarginWrapper';
+import copyButtonIcon from 'assets/img/copy_icon_white.svg';
+import { CopyButtonProps, CustomCopyButton } from 'components/common/buttons/CopyButton';
 import { ImgButton } from 'components/common/buttons/ImgButton';
 import { CustomImg } from 'components/common/imageComponents/CustomImg';
-import { copyButtonIconDiameter } from './consntants';
-import copyButtonIcon from 'assets/img/copy_button_icon.svg';
+import { MarginWrapper } from 'components/grid/wrappers/MarginWrapper';
+import { blue, blue4, blue8 } from 'constants/styles';
 import React from 'react';
-import { CopyButtonProps, CustomCopyButton } from 'components/common/buttons/CopyButton';
+import { BackgroundColor, Color } from 'types';
+import { copyButtonIconDiameter } from './consntants';
+import { CopyableFieldSpan, CopyableFieldWrapper } from './styles';
 
 interface CopyableFieldProps extends BackgroundColor, Color, Partial<Omit<CopyButtonProps, 'customCopyIcon'>> {}
 
@@ -17,14 +17,14 @@ export const CopyableField = ({
     subject,
     success = 'Copied successfully'
 }: CopyableFieldProps) => (
-    <CopyableFieldWrapper backgroundColor={backgroundColor} padding="8px">
-        <CopyableFieldSpan color={color}>{subject}</CopyableFieldSpan>
-        <MarginWrapper marginLeft="10px">
-            <CustomCopyButton subject={subject} success={success}>
-                <ImgButton backgroundColor={blue5}>
-                    <CustomImg height={copyButtonIconDiameter} src={copyButtonIcon} width={copyButtonIconDiameter} />
-                </ImgButton>
-            </CustomCopyButton>
+    <CopyableFieldWrapper backgroundColor={backgroundColor} padding="2px">
+        <MarginWrapper margin="0 10px">
+            <CopyableFieldSpan color={color}>{subject}</CopyableFieldSpan>
         </MarginWrapper>
+        <CustomCopyButton subject={subject} success={success}>
+            <ImgButton backgroundColor={blue8}>
+                <CustomImg height={copyButtonIconDiameter} src={copyButtonIcon} width={copyButtonIconDiameter} />
+            </ImgButton>
+        </CustomCopyButton>
     </CopyableFieldWrapper>
 );
